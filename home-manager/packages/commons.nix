2f5db1d93db36e6ddfb40db8846e44b05d1484b1
TobMoeller <tobiasmoellerw@t-online.de>
2024-01-28 22:49:01 +0100

    fix context commentstring, enable fzf, bring back t script

--- home-manager/packages/commons.nix
+++ home-manager/packages/commons.nix
@@ -27,6 +27,32 @@
 
     # LSP packages
     phpactor
+
+    # scripts TODO separate
+    (pkgs.writeScriptBin "t" ''
+#!/usr/bin/env bash
+
+# Credit to ThePrimeagen
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    items+=`find ~/code -maxdepth 2 -mindepth 1 -type d`
+    items+=`echo -e "\n$(find ~/portal -maxdepth 2 -mindepth 1 -type d)"`
+    items+=`echo -e "\n/tmp"`
+    selected=`echo "$items" | fzf`
+    # echo "$items"
+fi
+
+dirname=`basename $selected | sed 's/\./_/g'`
+
+tmux switch-client -t =$dirname
+if [[ $? -eq 0 ]]; then
+    exit 0
+fi
+
+tmux new-session -c $selected -d -s $dirname && tmux switch-client -t $dirname || tmux new -c $selected -A -s $dirname
+    '')
   ];
 
   home.file = {
@@ -61,8 +87,7 @@
     enable = true;
   };
 
-  # programs.fzf = {
-  #   enable = true;
-  # };
-
+  programs.fzf = {
+    enable = true;
+  };
 }
