147479411f3c1239bcf6c2dcee1725658153d335
TobMoeller <tobiasmoellerw@t-online.de>
2024-04-07 18:36:28 +0200

    add scrolling and live grep for visual selection to telescope

--- home-manager/packages/config/neovim/telescope.lua
+++ home-manager/packages/config/neovim/telescope.lua
@@ -28,9 +28,11 @@ require('telescope').setup({
         -- ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
         ["<C-p>"] = lga_actions.quote_prompt(),
         ["<C-n>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-        ["<C-h>"] = "which_key",
+        -- ["<C-h>"] = "which_key",
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
+        ["<C-h>"] = actions.preview_scrolling_left,
+        ["<C-l>"] = actions.preview_scrolling_right,
       },
     },
     file_ignore_patterns = { '.git/' },
@@ -61,5 +63,6 @@ vim.keymap.set('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_fi
 vim.keymap.set('n', '<leader>F', [[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' })<CR>]])
 vim.keymap.set('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
 vim.keymap.set('n', '<leader>g', [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>]])
+vim.keymap.set('v', '<leader>g', require("telescope-live-grep-args.shortcuts").grep_word_under_cursor)
 vim.keymap.set('n', '<leader>h', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]])
 vim.keymap.set('n', '<leader>s', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]])
