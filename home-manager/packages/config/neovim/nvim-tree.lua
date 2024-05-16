babeace55fe8a3aa745a557f18d337f067adf406
TobMoeller <tobiasmoellerw@t-online.de>
2024-05-16 12:55:15 +0200

    add resize option to nvim tree

--- home-manager/packages/config/neovim/nvim-tree.lua
+++ home-manager/packages/config/neovim/nvim-tree.lua
@@ -1,4 +1,25 @@
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- remove a default
+  -- vim.keymap.del('n', '<C-]>', { buffer = bufnr })
+
+  -- override a default
+  vim.keymap.set('n', '-', ':NvimTreeResize -30<CR>', opts('Resize -30'))
+
+  -- add your mappings
+  vim.keymap.set('n', '+', ':NvimTreeResize +30<CR>', opts('Resize +30'))
+  ---
+end
+
 require('nvim-tree').setup({
+  on_attach = my_on_attach,
   git = {
     ignore = false,
   },
